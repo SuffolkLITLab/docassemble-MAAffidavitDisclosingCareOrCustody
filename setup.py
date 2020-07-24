21b7c42f249c700a9183d2b9add722e03660a938
System Administrator <qsteenhuis@gmail.com>
2020-07-24 11:17:10 -0400

    Fix issue when no matching courts; use latest version from 209A package

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MAAffidavitDisclosingCareOrCustody',
-      version='0.0.7',
+      version='0.0.8',
       description=('Standalone version of the Affidavit Concerning Care or Custody'),
       long_description='# docassemble.MAAffidavitDisclosingCareOrCustody\r\n\r\nStandalone version of the Affidavit Concerning Care or Custody\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
